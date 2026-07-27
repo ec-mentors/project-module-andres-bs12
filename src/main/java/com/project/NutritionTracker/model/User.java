19877e1f45259f4867b3a4cd09849a955a8ed5d8
AndresB <andres_bs@hotmail.com>
2026-07-27 10:55:00 +0200

    feat(model): add User entity with UUID and Lombok annotations

--- src/main/java/com/project/NutritionTracker/model/User.java
+++ src/main/java/com/project/NutritionTracker/model/User.java
@@ -1,4 +1,34 @@
 package com.project.NutritionTracker.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String passwordHash;
+    private LocalDateTime createdAt;
 }
+
