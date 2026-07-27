f9556bead067ea38f86f45737be76c7ffbf073f3
AndresB <andres_bs@hotmail.com>
2026-07-27 09:33:39 +0200

    Clean up scratch code in Entry model

--- src/main/java/com/project/NutritionTracker/model/Entry.java
+++ src/main/java/com/project/NutritionTracker/model/Entry.java
@@ -1,30 +1,5 @@
 package com.project.NutritionTracker.model;
 
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Entry {
-
-    public static void main(String[] args) {
-
-
-        List<Integer> nums = new ArrayList<>();
-        int n1 = 10, n2 = 20, n3 = 30;
-
-        nums.add(n1);
-        nums.add(n2);
-        nums.add(n3);
-
-        for (int i = 0; i <= nums.size(); i++){
-            if(nums.get(i).si) {
-        }
-
-
-
-
-    }
-
-
 
 }
