49c8b42edaed9d639bdc819c5f2b35b3bada6a08
AndresB <andres_bs@hotmail.com>
2026-07-30 14:58:52 +0200

    feat(user): update User entity and schema for Google OAuth and telegram(BE-28)

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE users (
     email VARCHAR(100) UNIQUE NOT NULL,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    telegram_chat_id BIGINT,
+    telegram_chat_id BIGINT UNIQUE,
     created_at TIMESTAMP DEFAULT NOW()
 );
 
