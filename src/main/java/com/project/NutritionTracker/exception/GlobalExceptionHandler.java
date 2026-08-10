bd7faa4895d97f666c7e397073d93d5e2bea9c1f
AndresB <andres_bs@hotmail.com>
2026-08-10 10:26:21 +0200

    feat: Global exception handler added

---
+++ src/main/java/com/project/NutritionTracker/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.project.NutritionTracker.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // Catch NFE and -> Returns 404 Not Found
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleNotFound(NotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    // Catch IAE and -> Returns 400 Not Found
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+}
+
