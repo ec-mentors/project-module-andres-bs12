a7cd9bbea31626992018454e8e1fd69796cb1e33
AndresB <andres_bs@hotmail.com>
2026-08-30 13:38:42 +0200

    feat: AIQuotaDTO  and DailyUsage added:

--- src/main/java/com/project/NutritionTracker/model/DailyAiUsage.java
+++ src/main/java/com/project/NutritionTracker/model/DailyAiUsage.java
@@ -1,5 +1,33 @@
 package com.project.NutritionTracker.model;
 
+import com.google.api.client.util.DateTime;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class DailyAiUsage {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @JoinColumn(name = "user_id", nullable = false)
+    @ManyToOne
+    private User user;
+
+    private LocalDate usage_date;
+    private Integer goalsUsed;
+    private Integer favoritesUsed;
+    private int entries_used;
 }
